518455edcba8a60de9603e3bbabaa58030d9fca8
Kyle Fuller <kyle@fuller.li>
2015-03-28 16:05:14 +0000

    Initial project

---
+++ Inquiline/Inquiline.h
@@ -0,0 +1,19 @@
+//
+//  Inquiline.h
+//  Inquiline
+//
+//  Created by Kyle Fuller on 28/03/2015.
+//  Copyright (c) 2015 Cocode. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Inquiline.
+FOUNDATION_EXPORT double InquilineVersionNumber;
+
+//! Project version string for Inquiline.
+FOUNDATION_EXPORT const unsigned char InquilineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Inquiline/PublicHeader.h>
+
+
