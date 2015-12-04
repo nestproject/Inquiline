38dc6346ff033f00fb207f2e67fe4ac6adf0d005
Kyle Fuller <kyle@fuller.li>
2015-12-04 14:59:38 +0000

    Add support for Swift Package Manager

---
+++ Package.swift
@@ -0,0 +1,8 @@
+import PackageDescription
+
+let package = Package(
+  name: "Inquiline",
+  dependencies: [
+    .Package(url: "https://github.com/nestproject/Nest.git", majorVersion: 0, minor: 3),
+  ]
+)
